e416837e5186e2c0caa6be790c37e34551237a7c
Matthew Pagan <mastapegs01@gmail.com>
2021-10-06 15:34:58 -0400

    feat: async tests calling pokeapi working

--- pokeapi-types/src/lib.rs
+++ pokeapi-types/src/lib.rs
@@ -4,4 +4,14 @@ mod tests {
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[tokio::test]
+    async fn async_test_works() {
+        let response = reqwest::get("https://pokeapi.co/api/v2/pokemon/1/")
+            .await
+            .unwrap();
+        assert!(&response.status().is_success());
+        let json: serde_json::Value = response.json().await.unwrap();
+        assert_eq!(json["name"], "bulbasaur");
+    }
 }
