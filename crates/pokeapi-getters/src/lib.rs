3ccd0a35e1821d2cbaae8bbda8a119a12397f361
Matthew Pagan <mastapegs01@gmail.com>
2021-10-07 15:40:25 -0400

    feat: add documentation (#2)

--- crates/pokeapi-getters/src/lib.rs
+++ crates/pokeapi-getters/src/lib.rs
@@ -1,3 +1,8 @@
+//! # pokeapi-getters
+//!
+//! `pokeapi-getters` is a collection of async getter functions that assist
+//! in the retrieval of data from the [PokeAPI](https://pokeapi.co/).
+
 mod get_pokemon;
 
 pub use get_pokemon::get_pokemon;
