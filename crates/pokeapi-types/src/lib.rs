1fe6de7883d4f58901f356cd4504e0ee17817a19
Matthew Pagan <mastapegs01@gmail.com>
2021-10-07 15:36:53 -0400

    feat: add documentation

--- crates/pokeapi-types/src/lib.rs
+++ crates/pokeapi-types/src/lib.rs
@@ -1,3 +1,8 @@
+//! # pokeapi-types
+//!
+//! `pokeapi-types` is a collection of structs used to aid in the creation of
+//! data from the [PokeAPI](https://pokeapi.co/) service.
+
 mod pokemon;
 
 pub use pokemon::Pokemon;
