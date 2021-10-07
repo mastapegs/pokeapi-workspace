5cc31f5d44c251f3f44c5ffc531261991b01820c
Matthew Pagan <mastapegs01@gmail.com>
2021-10-06 22:02:42 -0400

    feat: big conversion of pokemon types

--- pokeapi-types/src/lib.rs
+++ pokeapi-types/src/lib.rs
@@ -1,6 +1,37 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct PokemonMoveVersion {
+    pub move_learn_method: NamedAPIResource,
+    pub version_group: NamedAPIResource,
+    pub level_learned_at: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PokemonMove {
+    pub r#move: NamedAPIResource,
+    pub version_group_details: Vec<PokemonMoveVersion>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PokemonHeldItemVersion {
+    pub version: NamedAPIResource,
+    pub rarity: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PokemonHeldItem {
+    pub item: NamedAPIResource,
+    pub version_details: Vec<PokemonHeldItemVersion>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct VersionGameIndex {
+    pub game_index: u32,
+    pub version: NamedAPIResource,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct NamedAPIResource {
     pub name: String,
     pub url: String,
@@ -17,12 +48,17 @@ pub struct PokemonAbility {
 pub struct Pokemon {
     pub abilities: Vec<PokemonAbility>,
     pub base_experience: u32,
+    pub forms: Vec<NamedAPIResource>,
+    pub game_indices: Vec<VersionGameIndex>,
     pub height: u32,
+    pub held_items: Vec<PokemonHeldItem>,
     pub id: u32,
     pub is_default: bool,
     pub location_area_encounters: String,
+    pub moves: Vec<PokemonMove>,
     pub name: String,
     pub order: u32,
+    pub species: NamedAPIResource,
     pub weight: u32,
 }
 
