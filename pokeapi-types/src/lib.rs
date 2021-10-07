4de042ebd314eeb098e5a5fe6f3c4f292af9c52b
Matthew Pagan <mastapegs01@gmail.com>
2021-10-06 22:10:33 -0400

    feat: completed typing pokemon

--- pokeapi-types/src/lib.rs
+++ pokeapi-types/src/lib.rs
@@ -1,6 +1,31 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct PokemonType {
+    pub slot: u8,
+    pub r#type: NamedAPIResource,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PokemonStat {
+    pub stat: NamedAPIResource,
+    pub effort: u32,
+    pub base_stat: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PokemonSprites {
+    pub front_default: Option<String>,
+    pub front_shiny: Option<String>,
+    pub front_female: Option<String>,
+    pub front_shiny_female: Option<String>,
+    pub back_default: Option<String>,
+    pub back_shiny: Option<String>,
+    pub back_female: Option<String>,
+    pub back_shiny_female: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct PokemonMoveVersion {
     pub move_learn_method: NamedAPIResource,
     pub version_group: NamedAPIResource,
@@ -59,6 +84,9 @@ pub struct Pokemon {
     pub name: String,
     pub order: u32,
     pub species: NamedAPIResource,
+    pub sprites: PokemonSprites,
+    pub stats: Vec<PokemonStat>,
+    pub types: Vec<PokemonType>,
     pub weight: u32,
 }
 
