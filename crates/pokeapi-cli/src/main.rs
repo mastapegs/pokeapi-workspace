9749da8ff0b21bbdd29fd8b70670aeb7cf110a65
Matthew Pagan <mastapegs01@gmail.com>
2021-10-07 23:04:01 -0400

    initial use of getters library

--- crates/pokeapi-cli/src/main.rs
+++ crates/pokeapi-cli/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use pokeapi_getters::get_pokemon;
+
+#[tokio::main]
+async fn main() {
+    let pokemon = get_pokemon(13).await;
+    println!("The pokemon retrieved is: {name}", name = pokemon.name);
 }
