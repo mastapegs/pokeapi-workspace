55865f71b765acf66f932df79fd9a47fcb95cf3a
Matthew Pagan <mastapegs01@gmail.com>
2021-10-08 09:41:27 -0400

    CLI ready

--- crates/pokeapi-cli/src/main.rs
+++ crates/pokeapi-cli/src/main.rs
@@ -1,7 +1,12 @@
 use pokeapi_getters::get_pokemon;
+use std::env;
 
 #[tokio::main]
 async fn main() {
-    let pokemon = get_pokemon(13).await;
+    let args: Vec<String> = env::args().collect();
+
+    let pokemon_id = &args[1].parse::<u32>().unwrap();
+    let pokemon = get_pokemon(*pokemon_id).await;
+
     println!("The pokemon retrieved is: {name}", name = pokemon.name);
 }
