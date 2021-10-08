20211b1b8083c1e432be04818a8f4b411b2db3da
Matthew Pagan <mastapegs01@gmail.com>
2021-10-08 14:24:00 -0400

    added pokemon form sprites

--- crates/pokeapi-types/src/pokemon_form.rs
+++ crates/pokeapi-types/src/pokemon_form.rs
@@ -1,6 +1,14 @@
 use crate::NamedAPIResource;
 use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PokemonFormSprites {
+    pub front_default: String,
+    pub front_shiny: String,
+    pub back_default: String,
+    pub back_shiny: String,
+}
+
 /// This is the JSON struct for the endpoint /pokemon-form/{id}
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PokemonForm {
@@ -12,6 +20,8 @@ pub struct PokemonForm {
     pub is_mega: bool,
     pub name: String,
     pub order: usize,
+    pub pokemon: NamedAPIResource,
+    pub sprites: PokemonFormSprites,
 }
 
 #[cfg(test)]
