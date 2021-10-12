d4a57eecb2e9087f9aa55bc954f7648b0f816396
Matthew Pagan <mastapegs01@gmail.com>
2021-10-12 18:20:59 -0400

    feat: better CSS

--- crates/dominator-front-end/src/lib.rs
+++ crates/dominator-front-end/src/lib.rs
@@ -1,9 +1,8 @@
-use wasm_bindgen::prelude::*;
-use std::sync::Arc;
-use once_cell::sync::Lazy;
+use dominator::{class, clone, events, html, Dom};
 use futures_signals::signal::{Mutable, SignalExt};
-use dominator::{Dom, class, html, clone, events};
-
+use once_cell::sync::Lazy;
+use std::sync::Arc;
+use wasm_bindgen::prelude::*;
 
 struct App {
     counter: Mutable<i32>,
@@ -18,21 +17,35 @@ impl App {
 
     fn render(state: Arc<Self>) -> Dom {
         // Define CSS styles
-        static ROOT_CLASS: Lazy<String> = Lazy::new(|| class! {
-            .style("display", "inline-block")
-            .style("background-color", "black")
-            .style("padding", "10px")
+        static ROOT_CLASS: Lazy<String> = Lazy::new(|| {
+            class! {
+                .style("display", "block")
+                .style("min-height", "100vh")
+                .style("background-color", "black")
+                .style("color", "white")
+            }
         });
 
-        static TEXT_CLASS: Lazy<String> = Lazy::new(|| class! {
-            .style("color", "white")
-            .style("font-weight", "bold")
+        static CONTAINER_CLASS: Lazy<String> = Lazy::new(|| {
+            class! {
+                .style("display", "flex")
+                .style("justify-content", "center")
+                .style("align-items", "center")
+            }
+        });
+        
+        static TEXT_CLASS: Lazy<String> = Lazy::new(|| {
+            class! {
+                .style("font-weight", "bold")
+            }
         });
 
-        static BUTTON_CLASS: Lazy<String> = Lazy::new(|| class! {
-            .style("display", "block")
-            .style("width", "100px")
-            .style("margin", "5px")
+        static BUTTON_CLASS: Lazy<String> = Lazy::new(|| {
+            class! {
+                .style("display", "block")
+                .style("width", "100px")
+                .style("margin", "5px")
+            }
         });
 
         // Create the DOM nodes
@@ -41,41 +54,43 @@ impl App {
 
             .children(&mut [
                 html!("div", {
-                    .class(&*TEXT_CLASS)
-                    .text_signal(state.counter.signal().map(|x| format!("Counter: {}", x)))
-                }),
-
-                html!("button", {
-                    .class(&*BUTTON_CLASS)
-                    .text("Increase")
-                    .event(clone!(state => move |_: events::Click| {
-                        // Increment the counter
-                        state.counter.replace_with(|x| *x + 1);
-                    }))
-                }),
-
-                html!("button", {
-                    .class(&*BUTTON_CLASS)
-                    .text("Decrease")
-                    .event(clone!(state => move |_: events::Click| {
-                        // Decrement the counter
-                        state.counter.replace_with(|x| *x - 1);
-                    }))
-                }),
-
-                html!("button", {
-                    .class(&*BUTTON_CLASS)
-                    .text("Reset")
-                    .event(clone!(state => move |_: events::Click| {
-                        // Reset the counter to 0
-                        state.counter.set_neq(0);
-                    }))
-                }),
+                    .class(&*CONTAINER_CLASS)
+                    
+                    .children(&mut [
+                        html!("div", {
+                            .class(&*TEXT_CLASS)
+                            .text_signal(state.counter.signal().map(|x| format!("Counter: {}", x)))
+                        }),
+                        html!("button", {
+                            .class(&*BUTTON_CLASS)
+                            .text("Increase")
+                            .event(clone!(state => move |_: events::Click| {
+                                // Increment the counter
+                                state.counter.replace_with(|x| *x + 1);
+                            }))
+                        }),
+                        html!("button", {
+                            .class(&*BUTTON_CLASS)
+                            .text("Decrease")
+                            .event(clone!(state => move |_: events::Click| {
+                                // Decrement the counter
+                                state.counter.replace_with(|x| *x - 1);
+                            }))
+                        }),
+                        html!("button", {
+                            .class(&*BUTTON_CLASS)
+                            .text("Reset")
+                            .event(clone!(state => move |_: events::Click| {
+                                // Reset the counter to 0
+                                state.counter.set_neq(0);
+                            }))
+                        }),
+                    ])
+                })
             ])
         })
     }
 }
-
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
