a81d86727be8e4fb97be208b86bc3cc484d1fe0f
Matthew Pagan <mastapegs01@gmail.com>
2021-10-12 19:26:18 -0400

    feat: improve rust server (#13)

--- crates/rocket-pokeapi/src/main.rs
+++ crates/rocket-pokeapi/src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use] extern crate rocket;
 
 use pokeapi_getters::get_pokemon;
+use pokeapi_types::Pokemon;
+use rocket::serde::json::Json;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,8 +10,8 @@ fn index() -> &'static str {
 }
 
 #[get("/pokemon/<id>")]
-async fn pokemon(id: &str) -> String {
-    get_pokemon(id.parse::<u32>().unwrap()).await.name
+async fn pokemon(id: &str) -> Json<Pokemon> {
+    Json(get_pokemon(id.parse::<u32>().unwrap()).await)
 }
 
 #[launch]
